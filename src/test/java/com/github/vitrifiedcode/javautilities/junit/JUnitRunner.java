a4dc5db815cf5bbe3c580d5afcefa36465277215
hyfloac <hyfloac@gmail.com>
2017-05-10 13:57:30 -0500

    Cause Travis-CI to fail when JUnit fails.

--- src/test/java/com/github/vitrifiedcode/javautilities/junit/JUnitRunner.java
+++ src/test/java/com/github/vitrifiedcode/javautilities/junit/JUnitRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.notification.Failure;
 
 public class JUnitRunner
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         Result result = JUnitCore.runClasses(MathTests.class, StringTests.class);
         for(Failure failure : result.getFailures())
@@ -14,5 +14,8 @@ public class JUnitRunner
             System.out.println(failure.getMessage());
             System.out.println(failure.toString());
         }
+
+        /* This is so that a Travis-CI build fails when a JUnit test fails. */
+        if(result.getFailureCount() > 0) { throw new Exception("JUnit test has failed."); }
     }
 }
