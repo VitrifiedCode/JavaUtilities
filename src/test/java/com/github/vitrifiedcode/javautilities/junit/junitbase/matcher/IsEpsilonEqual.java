49b97d177a2a16435ba7319156881ef2b161517d
hyfloac <hyfloac@gmail.com>
2017-05-11 14:26:25 -0500

    Sin tests.

---
+++ src/test/java/com/github/vitrifiedcode/javautilities/junit/junitbase/matcher/IsEpsilonEqual.java
@@ -0,0 +1,63 @@
+package com.github.vitrifiedcode.javautilities.junit.junitbase.matcher;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+
+public class IsEpsilonEqual extends BaseMatcher<Double>
+{
+    private final double expectedValue;
+    private final double epsilon;
+
+    public IsEpsilonEqual(double expectedValue, double epsilon)
+    {
+        this.expectedValue = expectedValue;
+        this.epsilon = epsilon;
+    }
+
+    public IsEpsilonEqual(double expectedValue) { this(expectedValue, 1.0E-5D); }
+
+    @Override
+    public boolean matches(Object actualValue)
+    {
+        return actualValue instanceof Double && areEqual((Double) actualValue, expectedValue, epsilon);
+    }
+
+    @Override
+    public void describeTo(Description description)
+    {
+        description.appendValue(expectedValue).appendValue(epsilon);
+    }
+
+    private static boolean areEqual(double actual, double expected, double epsilon)
+    {
+        if(Double.isNaN(actual)) { return Double.isNaN(expected); }
+
+        return Math.abs(actual - expected) < epsilon;
+    }
+
+    @Factory
+    public static Matcher<Double> equalTo(double operand, double epsilon)
+    {
+        return new IsEpsilonEqual(operand, epsilon);
+    }
+
+    @Factory
+    public static Matcher<Double> equalTo(double operand)
+    {
+        return new IsEpsilonEqual(operand);
+    }
+
+    @Factory
+    public static Matcher<Double> equalTo(float operand, float epsilon)
+    {
+        return new IsEpsilonEqual(operand, epsilon);
+    }
+
+    @Factory
+    public static Matcher<Double> equalTo(float operand)
+    {
+        return new IsEpsilonEqual(operand);
+    }
+}
---
+++ src/test/java/com/github/vitrifiedcode/javautilities/junit/junitbase/matcher/IsValueEqual.java
@@ -0,0 +1,46 @@
+package com.github.vitrifiedcode.javautilities.junit.junitbase.matcher;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+
+public class IsValueEqual<T> extends BaseMatcher<T>
+{
+    private final T expectedValue;
+
+    public IsValueEqual(T expectedValue)
+    {
+        this.expectedValue = expectedValue;
+    }
+
+    @Override
+    public boolean matches(Object actualValue)
+    {
+        if(expectedValue == null) { return actualValue == null; }
+        if(actualValue == null) { return false; }
+        if(expectedValue == actualValue || expectedValue.equals(actualValue)) { return true; }
+        if(expectedValue instanceof String && actualValue instanceof Character) { return ((String) expectedValue).equals(new String(new char[] { (Character) actualValue })); }
+        if(actualValue instanceof String && expectedValue instanceof Character) { return ((String) actualValue).equals(new String(new char[] { (Character) expectedValue })); }
+        if(expectedValue instanceof Number && actualValue instanceof Number)
+        {
+            double x = ((Number) expectedValue).doubleValue();
+            double y = ((Number) actualValue).doubleValue();
+            return x == y || (Double.isNaN(x) && Double.isNaN(y));
+        }
+
+        return false;
+    }
+
+    @Override
+    public void describeTo(Description description)
+    {
+        description.appendValue(expectedValue);
+    }
+
+    @Factory
+    public static <T> Matcher<T> equalTo(T operand)
+    {
+        return new IsValueEqual<T>(operand);
+    }
+}
