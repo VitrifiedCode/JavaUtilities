198b630f049c8e0b5ebc41717e1368408d4687e4
hyfloac <hyfloac@gmail.com>
2017-06-02 11:59:43 -0500

    Test multiplication vs bit shift.

--- src/jmh/java/com/github/vitrifiedcode/javautilities/test/Benchmarker.java
+++ src/jmh/java/com/github/vitrifiedcode/javautilities/test/Benchmarker.java
@@ -1,84 +1,26 @@
 package com.github.vitrifiedcode.javautilities.test;
 
-import com.github.vitrifiedcode.javautilities.array.ArrayUtil;
 import com.github.vitrifiedcode.javautilities.other.RandomUtil;
-import com.github.vitrifiedcode.javautilities.string.StaticPattern;
 import org.openjdk.jmh.annotations.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
 public class Benchmarker
 {
-    public static final double x = (double) (Math.random() * 5_000_000);
-    public static final double y = (double) (Math.random() * 5_000_000);
+    static final int x = RandomUtil.getRandomInt(10, 3948636);
 
-    public static final String STR = RandomUtil.getRandomString(50, 100);
-
-    public static final Pattern PATTERN = Pattern.compile(";");
-
-    //    @Benchmark
-    public String replaceCompiled()
+    @Benchmark
+    public int mul()
     {
-        return PATTERN.matcher(STR).replaceAll("hi");
-    }
-
-    //    @Benchmark
-    public String replace()
-    {
-        return STR.replaceAll(";", "hi");
-    }
-
-    //    @Benchmark
-    public String replaceStaticPattern()
-    {
-        return StaticPattern.getPattern(";").matcher(STR).replaceAll("hi");
-    }
-
-
-    static List<Integer> l0 = new ArrayList<>();
-    static List<Integer> l1 = new ArrayList<>();
-
-    static
-    {
-        for(int i = 0; i < 27990; ++i)
-        {
-            l0.add(RandomUtil.getRandomInt());
-            l1.add(RandomUtil.getRandomInt());
-        }
-
-        for(int i = 0; i < 100; ++i)
-        {
-            l1.add(RandomUtil.getRandomInt());
-        }
+        return x * 2;
     }
 
     @Benchmark
-    public int[] toPrimArr0()
+    public int shift()
     {
-        return ArrayUtil.toPrimitive(l0.toArray(new Integer[l0.size()]));
-    }
-
-    @Benchmark
-    public int[] toPrimArr1()
-    {
-        return l0.stream().mapToInt(v -> v).toArray();
-    }
-
-    @Benchmark
-    public int[] toPrimArr2()
-    {
-        return ArrayUtil.toPrimitive(l1.toArray(new Integer[l1.size()]));
-    }
-
-    @Benchmark
-    public int[] toPrimArr3()
-    {
-        return l1.stream().mapToInt(v -> v).toArray();
+        return x << 1;
     }
 }
