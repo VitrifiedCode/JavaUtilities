91ab34c7c6454fdd5dca5f799c4cc5b70fbf98eb
hyfloac <hyfloac@gmail.com>
2017-06-27 21:40:22 -0500

    Pattern serialization.

--- src/jmh/java/com/github/vitrifiedcode/javautilities/test/Benchmarker.java
+++ src/jmh/java/com/github/vitrifiedcode/javautilities/test/Benchmarker.java
@@ -1,26 +1,43 @@
 package com.github.vitrifiedcode.javautilities.test;
 
-import com.github.vitrifiedcode.javautilities.other.RandomUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.SerializationUtils;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
 public class Benchmarker
 {
-    static final int x = RandomUtil.getRandomInt(10, 3948636);
+    static Pattern pattern = Pattern.compile("\\/\\*( *)[a-zA-Z0-9_]+( *)\\*\\/");
+    static Gson gson = new GsonBuilder()
+//                .excludeFieldsWithModifiers(Modifier.STATIC)
+//                .setPrettyPrinting()
+.create();
+    byte[] bb = SerializationUtils.serialize(pattern);
+    String s = gson.toJson(pattern);
+
 
     @Benchmark
-    public int mul()
+    public void createNew()
     {
-        return x * 2;
+        Pattern pattern = Pattern.compile("\\/\\*( *)[a-zA-Z0-9_]+( *)\\*\\/");
     }
 
     @Benchmark
-    public int shift()
+    public void deBytes()
     {
-        return x << 1;
+        Pattern pattern0 = SerializationUtils.deserialize(bb);
+    }
+
+    @Benchmark
+    public void deJson()
+    {
+        Pattern pattern1 = gson.fromJson(s, new TypeToken<Pattern>() { }.getType());
     }
 }
